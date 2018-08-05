2bb227dd0cd70fac5027750b3d4a9f595c4e78a8
mohammad.h.ali@kw.zain.com <mohammad.h.ali@kw.zain.com>
2018-08-05 05:05:31 -0700

    TASK14

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 # Your imports should be here
-
-
+from rest_framework.generics import ListAPIView
+from django.contrib.auth.models import LV
+from rest_framework import routers, serializers, viewsets
 # Complete me! I should be your APIListView
-class RestaurantListView():
-	...
+
+	class LV(serializers.HyperlinkedModelSerializer):
+        model = LV
+        fields = ('name', 'opening_time', 'closing_time')
